74e1cbf881abfc3ab1c2bd4579a556ff4f3e9306
Leonardo Rodrigues <37932932+leonardorodd@users.noreply.github.com>
2019-12-15 11:47:40 -0300

    Update flask_server.py

--- FIWARE/ThingsApps_Raspberrypi_Wi-Fi/moverServoMotor/flask_server.py
+++ FIWARE/ThingsApps_Raspberrypi_Wi-Fi/moverServoMotor/flask_server.py
@@ -14,5 +14,5 @@ def commandExecution():
     #'mover' is the command name registered for the device
     #'Moved successfully!' is the command result sent to IoT Agent
     return jsonify({'mover': 'Moved successfully!'})
-
+    #5000 is the default flask port
 if __name__ == '__main__': app.run(debug=True, host=raspberryIp)
